2761d93a0d855e3c9b5406dd11411ca85660c44c
Matt Babcock <obi1matty@gmail.com>
2022-10-05 16:08:21 -0500

    following along with live session

---
+++ script.js
@@ -0,0 +1,9 @@
+//alert("connected");
+//declare variables
+
+const user_input = document.getElementById("your_name").value;
+console.log(user_input);
+
+function sayHello(){
+    console.log("hi");
+}
